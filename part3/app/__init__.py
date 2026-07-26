1afad6d34b89a6cd4ef3aae8c7cc32d1528b3e1f
ghadi0alzhrani0 <ghadi0alzhrani@gmail.com>
2026-07-26 15:50:14 +0300

    Configure Part 3 application

---
+++ part3/app/__init__.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Initialize the HBnB Flask application."""
+
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_restx import Api
+
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+from app.api.v1.auth import api as auth_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.users import api as users_ns
+from app.services import facade
+
+
+def create_app(config_class="config.DevelopmentConfig"):
+    """Create and configure the Flask application."""
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+    facade.reset()
+
+    api = Api(
+        app,
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/"
+    )
+
+    api.add_namespace(auth_ns, path="/api/v1/auth")
+    api.add_namespace(users_ns, path="/api/v1/users")
+    api.add_namespace(amenities_ns, path="/api/v1/amenities")
+    api.add_namespace(places_ns, path="/api/v1/places")
+    api.add_namespace(reviews_ns, path="/api/v1/reviews")
+
+    return app
